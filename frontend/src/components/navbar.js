6e7960b9c2298ea8bb57da6335fe2da81de22b3c
CabraCrimes <benalexanderthegreat@gmail.com>
2023-08-22 15:34:45 +0200

    Reactive Navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,50 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "../styles/navbar.css";
 
 export const Navbar = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-        {/* Burger button */}
-        <button
-          className="navbar-toggler ms-4"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-        <div className="navbar-brand  ms-4 me-5">
-          <Link className="text-link" to="/">
-            <h1>Lets Cook</h1>
-          </Link>
-        </div>
-
-        <ul className=" navbar-nav me-auto mb-2 mb-lg-0 d-flex">
-          <li className="nav-item">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="navbar-brand  ms-4 me-5">
             <Link className="text-link" to="/">
-              Link
+              <h1>Lets Cook</h1>
             </Link>
-          </li>
-        </ul>
-
-        <form className=" d-flex justify-content-center" role="search">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            Search
+          </div>
+          {/* Burger button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded={!isNavCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-        </form>
+
+          <div
+            className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mb-2 mb-lg-0 mx-auto">
+              <form className="d-flex " role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </ul>
+            <div className="nav-item me-5">
+              <Link className="text-link " to="/">
+                Link
+              </Link>
+            </div>
+          </div>
         </div>
       </nav>
     </>
