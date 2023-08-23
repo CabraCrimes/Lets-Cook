4f2e25339f4552164d4d8706f23f5d8960beb40c
CabraCrimes <benalexanderthegreat@gmail.com>
2023-08-23 20:50:08 +0200

    GET API endpoint Created

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, Flask
+import requests
 
 api_bp = Blueprint('api', __name__)
 
-@api_bp.route('/api')
-def get_data():
-    data = {
-        'msg': 'API test', 
-        'data': [1,2,3,4,5]
-        }
+@api_bp.route('/recipes/<ingredient>')
+def get_recipes(ingredient):
+    app_id = '9528e0a9'
+    app_key = '5c5fe2ef71d1c1e1ef728246cb78f0e1'
+    api_url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}'
+    
+    response = requests.get(api_url)
+    data = response.json()
+    
     return jsonify(data)
+ 
