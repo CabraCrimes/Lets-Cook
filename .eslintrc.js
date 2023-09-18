e3cddbf2545abb5f67a9c3dbeee935ccec6ab2d0
CabraCrimes <benalexanderthegreat@gmail.com>
2023-09-18 23:37:18 +0200

    .env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
-module.exports = {
-    // ...other ESLint configuration...
+// module.exports = {
+//     // ...other ESLint configuration...
   
-    // Extend the ESLint configuration provided by react-scripts
-    extends: 'react-app',
+//     // Extend the ESLint configuration provided by react-scripts
+//     extends: 'react-app',
   
-    // Add any custom rules or overrides here
-    rules: {
-      // Add custom rules if needed
-    },
-  };
+//     // Add any custom rules or overrides here
+//     rules: {
+//       // Add custom rules if needed
+//     },
+//   };
